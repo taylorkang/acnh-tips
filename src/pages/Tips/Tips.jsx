5163207b7ef0a7a5c4886e39a5c8bbcd85a069da
taylorkang <taylor@myrobin.io>
2020-09-21 02:07:04 +0900

    touched up styling

--- src/pages/Tips/Tips.jsx
+++ src/pages/Tips/Tips.jsx
@@ -3,25 +3,16 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import { useFirebaseApp, useFirestore } from 'reactfire';
 import { useHistory } from 'react-router-dom';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 import 'firebase/database';
 import 'firebase/auth';
 import './Tips.css';
 
 //const db = useDatabase();
-
-const TipsChat = () => {
-  const isEmpty = (str) => {
-    return !str || 0 === str.length;
-  };
-
-  return (
-    <div className='text-center'>
-      <h1>Chat</h1>
-    </div>
-  );
-};
 
 function Tips() {
   const firebase = useFirebaseApp();
@@ -31,6 +22,37 @@ function Tips() {
   useEffect(() => {
     getMessages(db);
   }, []);
+
+  const TipsChat = () => {
+    const isEmpty = (str) => {
+      return !str || 0 === str.length;
+    };
+
+    return (
+      <div className='text-center'>
+        <h1>Chat</h1>
+        <Container className='chat-box'>
+          <div clasName='scrollable'>
+            {messageList.map((message) => {
+              return <div className='chat'>{message.data.message}</div>;
+            })}
+          </div>
+          <div className='pinned'>
+            <InputGroup>
+              <FormControl
+                placeholder="Recipient's username"
+                aria-label="Recipient's username"
+                aria-describedby='basic-addon2'
+              />
+              <InputGroup.Append>
+                <Button variant='outline-secondary'>Button</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </div>
+        </Container>
+      </div>
+    );
+  };
 
   const getMessages = async () => {
     const messagesSnapshot = await db.collection('messages').get();
